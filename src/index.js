c7e7f677b16d8f07891bb0d7d41f7ea41f59e3b4
Alexbardjo <alexbardjo@gmail.com>
2021-03-05 20:24:24 +0200

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+    const arrNumber = String(n).split('');
+    if (arrNumber[0] === '-'){
+        return Number(arrNumber.slice(1).reverse().join(''));
+
+    } else {
+        return Number(arrNumber.reverse().join(''));
+
+    }
+};
+
+
